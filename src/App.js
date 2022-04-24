499142b1654edf865ea1583b8fd59085e9dc6eb8
Cwong2255 <70491160+Cwong2255@users.noreply.github.com>
2022-04-24 19:52:54 -0400

    Further cleaning, resetting HTML boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <h1 className="text-3xl font-light underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-light underline">Hello world!!!</h1>;
 }
 
 export default App;
