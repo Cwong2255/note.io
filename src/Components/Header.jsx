bda44d33f30fa5448880cf29f0ae882201fa5774
Cwong2255 <70491160+Cwong2255@users.noreply.github.com>
2022-04-24 22:54:15 -0400

    Footer and Header mostly done, still need to implement dark mode and add appropriate icons and effects

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,25 @@
-import EventNoteIcon from "@mui/icons-material/EventNote";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import EventNoteOutlinedIcon from "@mui/icons-material/EventNoteOutlined";
 import React from "react";
 function Header() {
   return (
     <header>
-      <div className="bg-[#FFD32D] bg-opacity-90 py-5">
-        <EventNoteIcon />
-        <h1 className="text-3xl tracking-wide text-white mx-4 font-semibold">
-          Note.IO
-        </h1>
+      <div className="bg-[#FFD32D] bg-opacity-90 py-4 flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl tracking-wide text-white mx-4 font-semibold">
+            <EventNoteOutlinedIcon
+              fontSize="large"
+              sx={{ color: "#FFB72B" }}
+              className="hover:-rotate-12 transition-transform duration-300 -rotate-6"
+            />
+            <span className="mx-2">note.io</span>
+          </h1>
+        </div>
+
+        <button className="px-3 py-2 bg-[#A865C9] rounded-lg shadow-md mx-10">
+          <DarkModeIcon sx={{ color: "white" }} />
+          {/* <LightModeOutlinedIcon />         */}
+        </button>
       </div>
     </header>
   );
